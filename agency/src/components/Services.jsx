88cc6e518b919a677499965a2aa1878899edb634
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2025-12-31 10:12:15 +0530

    create service card ui

--- agency/src/components/Services.jsx
+++ agency/src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
+import ServiceCard from './ServiceCard'
 
 const Services = () => {
 
@@ -33,6 +34,12 @@ const Services = () => {
         <img src={assets.bgImage2} alt="bgImage2" className='absolute -top-110 -left-70 -z-1 dark:hidden' />
 
         <Title title='How can we help?' description='From strategy to execution, we craft digital solutions that move your business forward.'/>
+
+        <div className='flex flex-col md:grid grid-cols-2'>
+            {servicesData.map((service,index)=>(
+                <ServiceCard key={index} service={service} index={index} />
+            ))}
+        </div>
 
     </div>
   )
