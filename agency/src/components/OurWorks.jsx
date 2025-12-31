c3dd1b83299c8ef9d2bb014283ae48aba2ab46d8
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2025-12-31 13:45:39 +0530

    create OurWorks component

---
+++ agency/src/components/OurWorks.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Title from './Title'
+import assets from '../assets/assets'
+
+const OurWorks = () => {
+
+
+    const workData = [
+        {
+            title:'Mobile app marketing',
+            description:'We turn bold ideas into powerful digital solutions that connect...',
+            Image:assets.work_mobile_app
+        },
+         {
+            title:'Dashboard management',
+            description:'We turn bold ideas into powerful digital solutions that connect...',
+            Image:assets.work_dashboard_management
+        },
+        {
+            title:'Fitness app promotion',
+            description:'We turn bold ideas into powerful digital solutions that connect...',
+            Image:assets.work_fitness_app
+        }
+    ]
+
+  return (
+    <div id='our-work' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+
+        <Title title='Our latest work' description='Browse our portfolio of innovative digital projects that showcase creativity, performance, and results.'/>
+
+        <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl'>
+
+            {
+                workData.map((work,index) => (
+                    <div key={index} className='hover:scale-102 duration-500 transition-all cursor-pointer'>
+
+                        <img src={work.Image} alt="work.Image" className=' w-full rounded-xl ' />
+                        <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
+                        <p className='text-sm opacity-06 w-5/6'>{work.description}</p>
+
+                    </div>
+                ))
+            }
+
+        </div>
+    </div>
+  )
+}
+
+export default OurWorks
