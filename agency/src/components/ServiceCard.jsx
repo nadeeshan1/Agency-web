8e510cb7b589f59dd9c2eaa41cc6182c185e0e83
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2025-12-31 12:14:49 +0530

    handleMouseMove

--- agency/src/components/ServiceCard.jsx
+++ agency/src/components/ServiceCard.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const ServiceCard = ({service,index}) => {
 
-    const [position,setPosition] = React.useState({x:0,y:0});
+    const [position,setPosition] = useState({x:0,y:0});
+    const [visible,setVisible] = useState(false);
     
-  return (
-    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10'>
+    const divRef = useRef(null) // we are used useRef to directly access the DOM element like div
 
-        <div className='pointer-events-none blur-2xl rounded-full bg-linear-to-r from-blue-500 via-indigo-500 to-purple-500 w-75 h-75 absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70' style={{top:position.y - 150,left:position.x - 150}} />
+    const handleMouseMove = (e)=>{
+
+        const bound = divRef.current.getBoundingClientRect(); // this will give the position of the div
+        setPosition({x:e.clientX - bound.left,y:e.clientY - bound.top})
+    }
+
+  return (
+    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10' onMouseEnter={()=>setVisible(true)} onMouseLeave={()=>setVisible(false)} ref={divRef} onMouseMove={handleMouseMove}>
+
+        <div className={`pointer-events-none blur-2xl rounded-full bg-linear-to-r from-blue-500 via-indigo-500 to-purple-500 w-75 h-75 absolute z-0 transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70':'opacity-0'}`} style={{top:position.y - 150,left:position.x - 150}} />
 
             <div className='flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative'>
 
